1cb3859582600f7cc8b9f92e6f8c6810291e0cf0
justin.carvalheiro <justin.carvalheiro16@gmail.com>
2024-02-07 10:56:18 +0100

    💄update ui with bootstrap

--- creapix/app/forms.py
+++ creapix/app/forms.py
@@ -1,4 +1,9 @@
 from django import forms
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField()
+    file = forms.FileField(widget=forms.FileInput(
+        attrs={
+            "class": "form-control",
+            'accept': 'image/*',
+        }), label="Choisir une image"
+    )
